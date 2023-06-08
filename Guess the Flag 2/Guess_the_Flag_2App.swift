99ed983ea493c716a114ff59f638bfda8da95eaf
Khaled <khalidshiba95@gmail.com>
2023-06-08 10:15:48 +0200

    Initial Commit

---
+++ Guess the Flag 2/Guess_the_Flag_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_Flag_2App.swift
+//  Guess the Flag 2
+//
+//  Created by Khalid Shiba on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_Flag_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
